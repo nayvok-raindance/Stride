63f3d0676c66c92bba8e868a98931dc67f01e22f
nayvok <44442827+nayvok@users.noreply.github.com>
2024-01-10 14:33:50 +0500

    Add sise prop and font-bold

--- client/src/components/UI/Button/Button.tsx
+++ client/src/components/UI/Button/Button.tsx
@@ -1,22 +1,27 @@
 import {ButtonHTMLAttributes, FC} from 'react';
 
-const Button: FC<ButtonProps> = ({children, variant = 'primary', ...props}) => {
+const Button: FC<ButtonProps> = ({children, variant = 'primary', size = 'large', ...props}) => {
 
-    const baseClassName = "text-gray100 p-4 rounded-lg flex justify-center items-center gap-2 ease-out duration-300"
+    const baseClassName = "text-gray100 p-4 font-bold rounded-lg flex justify-center items-center gap-2 ease-out duration-300"
     const primaryClassName = "bg-blueDark hover:bg-blue"
     const secondaryClassName = "bg-purpleDark hover:bg-purple"
+    const largeClassName = "px-7 py-2"
+    const smallClassName = "p-4"
 
     return (
-        <button {...props}
-                className={`${baseClassName} ${variant === "primary" ? primaryClassName : secondaryClassName}`}
-        >
+        <button {...props} className={`
+            ${baseClassName} 
+            ${variant === "primary" ? primaryClassName : secondaryClassName}
+            ${size === "small" ? smallClassName : largeClassName}
+            `}>
             {children}
         </button>
     );
 };
 
 export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
-    variant?: 'primary' | 'secondary';
+    size?: 'small' | 'large';
+    variant?: 'primary' | 'secondary'
 }
 
 export default Button;
--- client/src/components/UI/LinkButton/LinkButton.tsx
+++ client/src/components/UI/LinkButton/LinkButton.tsx
@@ -1,15 +1,21 @@
 import {Link} from "react-router-dom";
-
 import {FC, ReactNode} from 'react';
 
-const LinkButton: FC<LinkButtonProps> = ({children, to, variant = 'secondary'}) => {
+const LinkButton: FC<LinkButtonProps> = ({children, to, variant = 'secondary', size = 'large'}) => {
 
-    const baseClassName = "text-gray100 p-4 rounded-lg flex justify-center items-center gap-2 ease-out duration-300"
+    const baseClassName = "text-gray100 font-bold rounded-lg flex justify-center items-center gap-2 ease-out duration-300"
     const secondaryClassName = "hover:bg-gray400"
     const primaryClassName = "bg-purpleDark hover:bg-purple"
+    const largeClassName = "px-7 py-2"
+    const smallClassName = "p-4"
+
 
     return (
-        <Link to={to} className={`${baseClassName} ${variant === "primary" ? primaryClassName : secondaryClassName}`}>
+        <Link to={to} className={`
+            ${baseClassName} 
+            ${variant === "primary" ? primaryClassName : secondaryClassName}
+            ${size === "small" ? smallClassName : largeClassName}
+            `}>
             {children}
         </Link>
     );
@@ -19,6 +25,7 @@ export interface LinkButtonProps {
     children?: ReactNode;
     to: string;
     variant?: 'primary' | 'secondary';
+    size?: 'small' | 'large'
 }
 
 export default LinkButton;
