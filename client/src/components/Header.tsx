49aabf2f07963a73413e8d271e591ff113b1131e
nayvok <44442827+nayvok@users.noreply.github.com>
2024-01-10 10:25:59 +0500

    Create basic Header

---
+++ client/src/components/Header.tsx
@@ -0,0 +1,18 @@
+import Logo from "../assets/Logo.svg";
+import LinkButton from "./UI/LinkButton/LinkButton.tsx";
+
+const Header = () => {
+    return (
+        <header className={'bg-gray700 flex justify-between items-center px-14 py-4'}>
+            <div>
+                <img src={Logo} alt="logo"/>
+            </div>
+            <div className={'flex gap-2 items-center'}>
+                <LinkButton to={'/signIn'}>Sign in</LinkButton>
+                <LinkButton to={'/signUp'} variant={'primary'}>Sign up</LinkButton>
+            </div>
+        </header>
+    );
+};
+
+export default Header;
