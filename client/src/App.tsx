e03378b4f5f67adda7abf5f689629431bc1ed0c2
nayvok <44442827+nayvok@users.noreply.github.com>
2024-01-27 13:47:10 +0500

    Add routing

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,19 @@
+import {Route, Routes} from "react-router-dom";
+import SignInPage from "./pages/SignInPage.tsx";
+import SignUpPage from "./pages/SignUpPage.tsx";
+import Header from "./components/Header.tsx";
+import WelcomePage from "./pages/WelcomePage.tsx";
+
 function App() {
 
     return (
         <>
-            
+            <Header/>
+            <Routes>
+                <Route path={'/'} element={<WelcomePage/>}/>
+                <Route path={'/signIn'} element={<SignInPage/>}/>
+                <Route path={'/signUp'} element={<SignUpPage/>}/>
+            </Routes>
         </>
     )
 }
